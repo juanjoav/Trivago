faaf9b10beff1a67fada6d4d15afe57823dccb18
harrisondiaz <65098800+harrisondiaz@users.noreply.github.com>
2020-09-12 17:13:59 -0500

    Holiwi

--- src/models/Hotel.java
+++ src/models/Hotel.java
@@ -11,5 +11,9 @@ public class Hotel {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Agradecido con el de arriba");
+        System.out.println("Perras");
+        int x = 5;
+        int y = 5;
+        System.out.println(x+y);
     }
 }
