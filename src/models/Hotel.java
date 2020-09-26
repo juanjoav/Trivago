b1c4361ce1d02d3b90d7b9bab1ff200e26f91b12
harrisondiaz <65098800+harrisondiaz@users.noreply.github.com>
2020-09-26 18:12:45 -0500

    Cambios menores para que a juan le actualice y no se pueda el proyecto V2.2 .

--- src/models/Hotel.java
+++ src/models/Hotel.java
@@ -28,6 +28,115 @@ public class Hotel {
 
     }
 
+    public Hotel() {
+        userTreeSet = new TreeSet<>();
+        rooms = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            rooms.add(new Room(i,Quality.REGULAR));
+        }
+    }
+
+    /**
+     * Para encontrar el total de habitaciones
+     * @return el numero total
+     */
+    public int getTotalRooms(){
+        return rooms.size();
+    }
+    /**
+     * mirar el total de habitaciones no ocupadas del establecimiento
+     * @return total de habitaciones disponibles
+     */
+    public int getEconomic(){
+        int counter = 0;
+        for (int i = 0; i < rooms.size(); i++) {
+            if(rooms.get(i).getQuality() == Quality.ECONOMIC){
+                counter++;
+            }
+        }
+        return counter;
+    }
+    /**
+     * mirar el total de habitaciones Regular del establecimiento
+     * @return total de habitaciones disponibles
+     */
+    public int getRegular(){
+        int counter = 0;
+        for (int i = 0; i < rooms.size(); i++) {
+            if(rooms.get(i).getQuality() == Quality.REGULAR){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    /**
+     * mirar el total de habitaciones Premium del establecimiento
+     * @return total de habitaciones disponibles
+     */
+    public int getPremium(){
+        int counter = 0;
+        for (int i = 0; i < rooms.size(); i++) {
+            if(rooms.get(i).getQuality() == Quality.PREMIUM){
+                counter++;
+            }
+        }
+        return counter;
+    }
+    /**
+     * mirar el total de habitaciones no ocupadas del establecimiento
+     * @return total de habitaciones disponibles
+     */
+    public int getAvailable(){
+        int counter = 0;
+        for (int i = 0; i < rooms.size(); i++) {
+            if(!rooms.get(i).isOcupation()){
+                counter++;
+            }
+        }
+        return counter;
+    }
+    /**
+     * mirar el total de habitaciones  ocupadas del establecimiento
+     * @return total de habitaciones disponibles
+     */
+    public int getDisavailable(){
+        int counter = 0;
+        for (int i = 0; i < rooms.size(); i++) {
+            if(rooms.get(i).isOcupation()){
+                counter++;
+            }
+        }
+        return counter;
+    }
+    /**
+     * Para meterle los numero automaticamente los numeros de cada habitacion
+     */
+    private void SetNumberRooms(){
+        int counter = 0;
+        for (int i = 0; i < rooms.size(); i++) {
+            counter++;
+            rooms.get(i).setNumber(counter);
+        }
+    }
+
+    /**
+     * Metodo para aumentar el numero de habitaciones
+     * @param room
+     */
+    public void add(Room room){
+        rooms.add(room);
+        SetNumberRooms();
+    }
+
+    /**
+     * Recupera el room para hacer modificaciones
+     * @param iterator numero de cual habitacion
+     * @return
+     */
+    public Room getRoom(int iterator){
+        return rooms.get(iterator);
+    }
 
     /**
      * Para decir que una habitacion esta fuera de serivicio
