466329cf6a82e4074cec77e8a9d0961a8c0f5f1a
juanjoav <57376589+juanjoav@users.noreply.github.com>
2020-09-26 10:29:48 -0500

    Se implemento la carpeta test y se agregan colecciones

--- src/models/Hotel.java
+++ src/models/Hotel.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 12/9/2020.
  * created by Mateo Pinzon, Harrison Diaz y Juan Jose Ariza
@@ -6,26 +7,25 @@
 
 package models;
 
+import java.util.TreeSet;
+
 public class Hotel {
 
-    private Person [] personList;
+    private TreeSet<User> userTreeSet;
 
     public Hotel() {
-        personList = new Person[]{};
+        userTreeSet = new TreeSet<User>();
     }
 
-    /**
-     * Agrega Usuarios
-     * @param user
-     */
-    public void addPerson(Person user){
-        Person [] aux = new Person[personList.length + 1];
-        System.arraycopy(personList,0,aux,0,personList.length);
-        aux[personList.length - 1] = user;
-        personList = aux;
+    public void addUser(User user){
+        userTreeSet.add(user);
+    }
+
+    public void view(){
+        System.out.println(userTreeSet);
     }
 
     public void viewUserInformation(){
-                                            //revision de ubicacion
+
     }
 }
