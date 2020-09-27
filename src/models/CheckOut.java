9715fdf0b842b6b3bd76245b07b5963bf134d702
harrisondiaz <65098800+harrisondiaz@users.noreply.github.com>
2020-09-26 19:39:11 -0500

    Se creo para la reservacion y sus metodos pertinentes.

--- src/models/CheckOut.java
+++ src/models/CheckOut.java
@@ -6,13 +6,22 @@
 
 package models;
 
+import java.time.LocalDate;
+
 public class CheckOut extends Event {
 
+    public CheckOut(LocalDate timeInitial, LocalDate timeFinish, User user, Room room) {
+        super(timeInitial, timeFinish, user, room);
+    }
+
+    public CheckOut(Event event){
+        super(event.getTimeInitial(),event.getTimeFinish(),event.getUser(), event.getRoom());
+    }
     /**
      * Metodo para asignar laas habitaciones
      * @param room para mirar cada habitacion
      */
-    public void assingRoom(Room room){
+    public void outRooms(Room room){
         room.setOcupation(false);
     }
 }
--- src/models/Event.java
+++ src/models/Event.java
@@ -9,16 +9,48 @@ package models;
 import java.time.LocalDate;
 
 
-public abstract class Event {
-    private LocalDate time;
+public class Event {
+    private LocalDate timeInitial;
+    private LocalDate timeFinish;
     private User user;
     private Room room;
 
-    /**
-     * GetTime nos retorna el dia del evento
-     * @return localdate value
-     */
-    public LocalDate getTime() {
-        return time;
+    public Event(LocalDate timeInitial, LocalDate timeFinish, User user, Room room) {
+        this.timeInitial = timeInitial;
+        this.timeFinish = timeFinish;
+        this.user = user;
+        this.room = room;
+    }
+
+    public LocalDate getTimeInitial() {
+        return timeInitial;
+    }
+
+    public void setTimeInitial(LocalDate timeInitial) {
+        this.timeInitial = timeInitial;
+    }
+
+    public LocalDate getTimeFinish() {
+        return timeFinish;
+    }
+
+    public void setTimeFinish(LocalDate timeFinish) {
+        this.timeFinish = timeFinish;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
     }
 }
